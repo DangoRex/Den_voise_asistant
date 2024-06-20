bbbd283aacda6965e6dde22c9418dcab422571ee
DangoRex <nastyatitar@mail.ru>
2024-06-20 16:05:35 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -8,15 +8,15 @@ function updateVersionInfo() {
 
     switch (selectedVersion) {
         case '1.0.0':
-            infoText = 'Информация о версии 1.0.0: В этой версии доступны функции:<br>  • Управление яркостью и звуком<br>  • Решение простых примеров<br>  • Получение времени<br>  • Включение автокликера<br>  • Открытие приложений<br>  • Выключение компьютера<br>  • Простое управление WASD<br>  • Создание скриншота';
+            infoText = 'Информация о версии 1.0.0: В этой версии доступны функции:<br>  • Управление яркостью и звуком<br>  • Решение простых примеров<br>  • Получение времени<br>  • Включение автокликера<br>  • Открытие приложений<br>  • Выключение компьютера<br>  • Создание скриншота';
             requirementsText = '<p>Минимальные системные требования:</p><ul><li>Операционная система: Windows</li><li>Оперативная память: 6,5 MБ</li><li>Место на диске: 3 ГБ</li><li>Микрофон</li><li>Динамик</li></ul>';
             break;
         case '1.1.0':
             infoText = 'Информация о версии 1.1.0: Добавлены функции:<br>  • Записи звука';
-            requirementsText = '<p>Минимальные системные требования:</p><ul><li>Операционная система: Windows</li><li>Оперативная память: X MБ</li><li>Место на диске: X ГБ</li><li>Микрофон</li><li>Динамик</li></ul>';
+            requirementsText = '<p>Минимальные системные требования:</p><ul><li>Операционная система: Windows</li><li>Оперативная память: 6,5 MБ</li><li>Место на диске: 3 ГБ</li><li>Микрофон</li><li>Динамик</li></ul>';
             break;
-        case '1.2.0':
-            infoText = 'Информация о версии 2.1.0: Добавлены функции:<br>  • Записи экрана';
+        case '2.0.0':
+            infoText = 'Информация о версии 2.0.0: ;
             requirementsText = '<p>Минимальные системные требования:</p><ul><li>Операционная система: Windows</li><li>Оперативная память: X MБ</li><li>Место на диске: X ГБ</li><li>Микрофон</li><li>Динамик</li></ul>';
             break;
         default:
@@ -36,7 +36,7 @@ function download() {
     const downloadLinks = {
         '1.0.0': 'https://drive.google.com/uc?export=download&id=1Y39qZnnwqASSP_54nF6Em-BGTpB07rb4',
         '1.1.0': 'https://drive.google.com/uc?export=download&id=1p1k8ikYe1dVmLPDnxRACeqoS8pSEv580',
-        '1.2.0': 'https://drive.google.com/uc?export=download&id=ID_ДЛЯ_ВЕРСИИ_1.2.0'  // Укажите правильный ID для версии 1.2.0
+        '2.0.0': 'https://drive.google.com/uc?export=download&id=ID_ДЛЯ_ВЕРСИИ_1.2.0'  // Укажите правильный ID для версии 1.2.0
     };
 
     const downloadLink = downloadLinks[selectedVersion] || '#';
