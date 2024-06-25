702fb2d580a822873a03db2a5f3bd9cefa6da3f5
DangoRex <nastyatitar@mail.ru>
2024-06-25 17:08:26 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -9,24 +9,24 @@ function updateVersionInfo() {
     switch (selectedVersion) {
         case '1.0.0':
             infoText = 'Информация о версии 1.0.0: В этой версии доступны функции:<br>  • Управление яркостью и звуком<br>  • Решение простых примеров<br>  • Получение времени<br>  • Включение автокликера<br>  • Открытие приложений<br>  • Выключение компьютера<br>  • Создание скриншота';
-            requirementsText = '<ul><li>Операционная система: Windows</li><li>Оперативная память: 6.5 МБ</li><li>Место на диске: 3 ГБ</li><li>Микрофон</li><li>Динамик</li></ul>';
+            requirementsText = '<p>Минимальные системные требования:</p><ul><li>Операционная система: Windows</li><li>Оперативная память: 6,5 MБ</li><li>Место на диске: 3 ГБ</li><li>Микрофон</li><li>Динамик</li></ul>';
             break;
         case '1.1.0':
             infoText = 'Информация о версии 1.1.0: Добавлены функции:<br>  • Записи звука';
-            requirementsText = '<ul><li>Операционная система: Windows</li><li>Оперативная память: 6.5 МБ</li><li>Место на диске: 6.5 ГБ</li><li>Микрофон</li><li>Динамик</li></ul>';
+            requirementsText = '<p>Минимальные системные требования:</p><ul><li>Операционная система: Windows</li><li>Оперативная память: 6,5 MБ</li><li>Место на диске: 6,5 ГБ</li><li>Микрофон</li><li>Динамик</li></ul>';
             break;
         case '2.0.0':
             infoText = 'Информация о версии 2.0.0: Уменьшен размер программы больше, чем в 2 раза, по сравнению с версией 1.1.0. Изменений в функционале нет.';
-            requirementsText = '<ul><li>Операционная система: Windows</li><li>Оперативная память: 6.3 МБ</li><li>Место на диске: 2.8 ГБ</li><li>Микрофон</li><li>Динамик</li></ul>';
+            requirementsText = '<p>Минимальные системные требования:</p><ul><li>Операционная система: Windows</li><li>Оперативная память: 6,3 MБ</li><li>Место на диске: 2,8 ГБ</li><li>Микрофон</li><li>Динамик</li></ul>';
             break;
         default:
             infoText = 'Выберите версию из списка, чтобы увидеть информацию.';
-            requirementsText = '<ul><li>Операционная система: Windows</li><li>Оперативная память: 6.3 МБ</li><li>Место на диске: 2.8 ГБ</li><li>Микрофон</li><li>Динамик</li></ul>';
+            requirementsText = '<p>Минимальные системные требования:</p><ul><li>Операционная система: Windows</li><li>Оперативная память: 6,3 MБ</li><li>Место на диске: 2,8 ГБ</li><li>Микрофон</li><li>Динамик</li></ul>';
             break;
     }
 
     versionInfo.innerHTML = `<p>${infoText}</p>`;
-    document.getElementById('systemRequirements').innerHTML = requirementsText;
+    document.getElementById('requirementsList').innerHTML = requirementsText;
 }
 
 function download() {
@@ -42,7 +42,7 @@ function download() {
     const downloadLink = downloadLinks[selectedVersion] || '#';
 
     if (downloadLink !== '#') {
-        window.open(downloadLink, '_blank');
+        window.location.href = downloadLink;
     } else {
         alert('Выберите версию для скачивания.');
     }
